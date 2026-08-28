7b73a44cc0e090e1ff76b8059f1a8f780aae9d6d
jlevine456 <jlevine456@users.noreply.github.com>
2026-08-28 07:24:31 -0400

    Use stdin for unbounded managed request payloads (#181)
    * Add stdin transport for variable managed request data
    
    * Advertise managed stdin request capability
    
    * Update discovery contract for stdin transport
    
    * Update shell discovery contract for stdin transport
    
    * Use schema-compatible stdin capability name
    
    * Match schema-compatible stdin capability name
    
    * Match schema-compatible stdin capability name
    
    * Test large managed stdin request expansion

--- src/jl_mixing/system_info.py
+++ src/jl_mixing/system_info.py
@@ -24,6 +24,7 @@ _CAPABILITIES = [
     "intake.validate.progress",
     "intake.validate.report",
     "intake.validate.structured",
+    "managed.requests.stdinjson",
     "project.create",
     "project.create.artist",
     "project.update",
